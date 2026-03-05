5505342afd9bcc4052f970a8ad788b251abfc562
seemsless <olivervilem@gmail.com>
2026-03-06 02:08:35 +0700

    I feel like I am at the point where I can implement features. I plan to add Roles and Permissions, Url slugs and a way to deal with media and files. Packages will help me allot. I also need to expand the database. (I removed one ! in the temporary text)

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="grid grid-cols-12 gap-4 md:gap-6">
         <div class="col-span-12 space-y-6 xl:col-span-12">
-            !!!!!!!!!!!!!!!!Comments!!!!!!!!!!!!!!!!
+            !!!!!!!!!!!!!!!!Comments!!!!!!!!!!!!!!!
             @foreach($comments as $comment)
                 <div class="border-b p-4">
                     <p><strong>{{ $comment->user->name }}</strong> said:</p>
