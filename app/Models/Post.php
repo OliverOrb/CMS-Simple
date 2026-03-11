12b045c181fd593594c177936a7abeaf715d19c4
seemsless <olivervilem@gmail.com>
2026-03-12 06:34:38 +0700

    Using CRUD I overrode Post.php and PostController.php and tweaked the .blade.php files. I do believe that doing it manually would come out better.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,23 +4,50 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Post
+ *
+ * @property $id
+ * @property $user_id
+ * @property $title
+ * @property $slug
+ * @property $body
+ * @property $image
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property User $user
+ * @property Comment[] $comments
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
 class Post extends Model
 {
-    protected $fillable = [
-        'user_id',
-        'title',
-        'slug',
-        'body',
-        'image'
-    ];
+    
+    protected $perPage = 20;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['user_id', 'title', 'slug', 'body', 'image'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(\App\Models\User::class, 'user_id', 'id');
     }
-
+    
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function comments()
     {
-        return $this->hasMany(Comment::class);
+        return $this->hasMany(\App\Models\Comment::class, 'id', 'post_id');
     }
+    
 }
