76d36c9f22832b09ba1d07a62f2390b21188a751
seemsless <olivervilem@gmail.com>
2026-03-26 00:53:18 +0700

    removed 'image'

--- database/migrations/2026_03_03_022233_create_posts_table.php
+++ database/migrations/2026_03_03_022233_create_posts_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('body');
-            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
