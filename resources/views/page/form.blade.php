78712917223724beca2949bd173d787595914bde
seemsless <olivervilem@gmail.com>
2026-03-26 00:54:17 +0700

    removed is_published and renamed content to body

--- resources/views/page/form.blade.php
+++ resources/views/page/form.blade.php
@@ -14,32 +14,17 @@
         @enderror
     </div>
 
-    {{-- Content Field --}}
+    {{-- Body Field --}}
     <div>
-        <label for="content" class="mb-3 block text-sm font-medium text-gray-800 dark:text-white/90">
-            {{ __('Content') }}
+        <label for="body" class="mb-3 block text-sm font-medium text-gray-800 dark:text-white/90">
+            {{ __('Body') }}
         </label>
-        <input id="content" name="content" type="text"
+        <input id="body" name="body" type="text"
                class="w-full rounded-lg border border-gray-200 bg-transparent px-5 py-3 font-medium text-gray-800 outline-none transition focus:border-brand-500 active:border-brand-500 dark:border-gray-800 dark:bg-white/[0.03] dark:text-white/90"
-               value="{{ old('content', $page?->content) }}"
-               placeholder="{{ __('Content') }}">
+               value="{{ old('body', $page?->body) }}"
+               placeholder="{{ __('Body') }}">
 
         @error('body')
-        <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-        @enderror
-    </div>
-
-    {{-- Is Published Field --}}
-    <div>
-        <label for="is_published" class="mb-3 block text-sm font-medium text-gray-800 dark:text-white/90">
-            {{ __('Is Published') }}
-        </label>
-        <input id="is_published" name="is_published" type="text"
-               class="w-full rounded-lg border border-gray-200 bg-transparent px-5 py-3 font-medium text-gray-800 outline-none transition focus:border-brand-500 active:border-brand-500 dark:border-gray-800 dark:bg-white/[0.03] dark:text-white/90"
-               value="{{ old('is_published', $page?->is_published) }}"
-               placeholder="{{ __('Is Published') }}">
-
-        @error('image')
         <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
         @enderror
     </div>
